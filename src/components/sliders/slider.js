e744ee6e2f43369329a681dd6fba733be29d8354
Rikard Vestergaard <rikve590@student.liu.se>
2023-04-04 11:30:37 +0200

    Added slider component

--- src/components/sliders/slider.js
+++ src/components/sliders/slider.js
@@ -1,13 +1,127 @@
-import './slider.css';
+import "./slider.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import { useState } from "react";
 
 const Slider = ({ props }) => {
-  const { header } = props;
+  const [index, setIndex] = useState(0);
+
+  const mapIndexToName = {
+    0: "Sågtand",
+    1: "Sinus",
+    2: "Fyrkant",
+  };
+
+  const listLength = 3;
+  const height = 60;
+  const arrowHeight = 25;
+  const arrowWidth = 20;
+
+  const handlePrev = () => {
+    const newIndex = index === 0 ? listLength - 1 : index - 1;
+    setIndex(newIndex);
+  };
+
+  const handleNext = () => {
+    const newIndex = index === listLength - 1 ? 0 : index + 1;
+    setIndex(newIndex);
+  };
   return (
-    <div className='sliderContainer'>
-      <div className='sliderHeader'>{header}</div>
-      <div className='sliderContent'>Put slider here</div>
+    <div className="sliderContainer">
+      <div className="sliderContent">
+        <div
+          onClick={handlePrev}
+          style={{
+            ...btnStyles,
+            height: height,
+            marginRight: "10%",
+          }}
+        >
+          <div>
+            <img
+              alt=""
+              src={require("../../assets/arrow.png")}
+              width={arrowWidth}
+              height={arrowHeight}
+            />
+          </div>
+        </div>
+        <Carousel
+          onChange={(e) => console.log(mapIndexToName[e])}
+          showArrows={false}
+          showThumbs={false}
+          showIndicators={false}
+          showStatus={false}
+          selectedItem={index}
+        >
+          <div
+            style={{
+              height,
+            }}
+          >
+            <img
+              src={require("../../assets/sawtooth.png")}
+              height={height}
+              alt=""
+            />
+          </div>
+
+          <div
+            style={{
+              height: height,
+            }}
+          >
+            <img
+              src={require("../../assets/sawtooth.png")}
+              height={height}
+              alt=""
+            />
+          </div>
+
+          <div
+            style={{
+              height: height,
+            }}
+          >
+            <img
+              src={require("../../assets/sawtooth.png")}
+              height={height}
+              alt=""
+            />
+          </div>
+        </Carousel>
+        <div
+          className="toggleBtn"
+          onClick={handleNext}
+          style={{
+            ...btnStyles,
+            height: height,
+            marginLeft: "10%",
+          }}
+        >
+          <div>
+            <img
+              src={require("../../assets/arrow-reverse.png")}
+              width={arrowWidth}
+              height={arrowHeight}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div style={{ fontWeight: "bold", color: "white", marginTop: "2%" }}>
+        {mapIndexToName[index]}
+      </div>
     </div>
   );
+};
+
+const btnStyles = {
+  cursor: "pointer",
+  justifyContent: "center",
+  alignItems: "center",
+  display: "flex",
+  width: "100%",
 };
 
 export default Slider;
