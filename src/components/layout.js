430e06fa91a0c4fc37b5065017445e78e501d8a8
F100P <tim.j.myllyla@gmail.com>
2023-04-12 14:34:05 +0200

    another test...

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,49 @@
-import Settings from "./settings/settings";
-import Graph from "./graph/graph";
-import { useContext } from "react";
-import WebSocketContext from "../utils/websocketContext";
-import { useDispatch, useSelector } from "react-redux";
-import { updateFlag } from "../redux/actions";
+import Settings from './settings/settings';
+import Graph from './graph/graph';
+import { useContext } from 'react';
+import WebSocketContext from '../utils/websocketContext';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateFlag } from '../redux/actions';
+import { randomInt } from 'd3';
 
 const Layout = () => {
-  const container = {
-    display: "flex",
-    flexDirection: "column",
-    height: "100vh",
-    width: "100%",
-  };
+	const container = {
+		display: 'flex',
+		flexDirection: 'column',
+		height: '100vh',
+		width: '100%',
+	};
 
-  const ws = useContext(WebSocketContext);
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
+	const ws = useContext(WebSocketContext);
+	const dispatch = useDispatch();
+	const state = useSelector((state) => state);
 
-  const sendMessage = () => {
-    // The message consist of our whole redux state
-    const message = {
-      dataset: state,
-    };
+	const sendMessage = () => {
+		// The message consist of our whole redux state
+		const message = {
+			dataset: state,
+		};
 
-    // Send the message as json to server, using the websocket service
-    ws.send(JSON.stringify(message));
-  };
+		// Send the message as json to server, using the websocket service
+		ws.send(JSON.stringify(message));
+	};
 
-  const toggle = () => {
-    // Example of how we can set a flag
-    // Takes in the variable name and 0 or 1 (on/off)
-    dispatch(updateFlag("amplitudeFlag", 1));
-  };
+	const toggle = () => {
+		// Example of how we can set a flag
+		// Takes in the variable name and 0 or 1 (on/off)
+		const rand = 0 + Math.random() * (10 - 0);
 
-  return (
-    <div style={container}>
-      {/* <Graph />
+		dispatch(updateFlag('graphValue', parseInt(rand)));
+	};
+
+	return (
+		<div style={container}>
+			{/* <Graph />
       <Settings /> */}
-      <button onClick={toggle}>Update amplitude flag</button>
-      <button onClick={sendMessage}>Send state to server</button>
-    </div>
-  );
+			<button onClick={toggle}>Update amplitude flag</button>
+			<button onClick={sendMessage}>Send state to server</button>
+		</div>
+	);
 };
 
 export default Layout;
