a18df6831b9bbfce2313103f08aeb6e28ca2bb20
F100P <tim.j.myllyla@gmail.com>
2023-04-16 16:00:46 +0200

    Visuals for profile is done

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,37 +1,42 @@
 import Settings from './settings/settings';
 import Graph from './graph/graph';
+import ProfileMenu from './profiles/profileMenu';
 
 const Layout = () => {
-  const container = {
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100vh',
-    width: '100%',
-    backgroundColor: '#DBDBDB',
-  };
+	const container = {
+		display: 'flex',
+		flexDirection: 'column',
+		height: '100vh',
+		width: '100%',
+		backgroundColor: '#DBDBDB',
+	};
 
-  return (
-    <div style={container}>
-      <div style={{ background: 'orange', flex: 1 }}>
-        <div style={{ fontWeight: 'bold', fontSize: 24 }}>
-          UTFORSKA SONIFIERING
-        </div>
-        <text>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum
-        </text>
-      </div>
-      <Graph />
-      <Settings />
-    </div>
-  );
+	return (
+		<div style={container}>
+			<div style={{ background: 'orange', flex: 1 }}>
+				<div style={{ fontWeight: 'bold', fontSize: 24 }}>
+					UTFORSKA SONIFIERING
+				</div>
+				<text>
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry. Lorem Ipsum has been the industry's standard dummy text ever
+					since the 1500s, when an unknown printer took a galley of type and
+					scrambled it to make a type specimen book. It has survived not only
+					five centuries, but also the leap into electronic typesetting,
+					remaining essentially unchanged. It was popularised in the 1960s with
+					the release of Letraset sheets containing Lorem Ipsum passages, and
+					more recently with desktop publishing software like Aldus PageMaker
+					including versions of Lorem Ipsum
+				</text>
+			</div>
+			<div style={{ display: 'flex' }}>
+				<Graph />
+				<ProfileMenu active={2} />
+			</div>
+
+			<Settings />
+		</div>
+	);
 };
 
 export default Layout;
---
+++ src/components/profiles/profileMenu.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+
+// const IoButton = ({ id, invId, color }) => {
+//   const [orientation, setOrientation] = useState(false);
+//   const dispatch = useDispatch();
+//   console.log(id);
+//   console.log(invId);
+
+//   function handleRing(e) {
+//     e.stopPropagation();
+//     setOrientation(orientation ? false : true);
+//   }
+
+//   const toggle = (e) => {
+//     // Example of how we can set a flag
+//     // Takes in the variable name and 0 or 1 (on/off)
+//     dispatch(updateFlag(id, e));
+//     dispatch(updateFlag(invId, e));
+//   };
+
+const ProfileMenu = ({ active }) => {
+	const [current, setCurrent] = useState(active);
+
+	const handleClick = (e) => {
+		console.log(e);
+		setCurrent(e);
+	};
+	return (
+		<div style={menuStyle}>
+			<div
+				style={current == 1 ? activeButton : profileButton}
+				onClick={() => handleClick(1)}
+			>
+				<p>1</p>
+			</div>
+			<div
+				style={current == 2 ? activeButton : profileButton}
+				onClick={() => handleClick(2)}
+			>
+				<p>2</p>
+			</div>
+			<div
+				style={current == 3 ? activeButton : profileButton}
+				onClick={() => handleClick(3)}
+			>
+				<p>3</p>
+			</div>
+		</div>
+	);
+};
+
+const menuStyle = {
+	display: 'flex',
+	justifyContent: 'center',
+	flexDirection: 'column',
+	with: '10vw',
+	marginTop: '1vw',
+};
+
+const profileButton = {
+	justifyContent: 'center',
+	textAlign: 'center',
+	lineHeight: '2vw',
+	display: 'block',
+	height: '6vw',
+	width: '6vw',
+	backgroundColor: '#A7A7A7',
+	borderRadius: '10%',
+	marginBottom: '-1vw',
+	fontSize: '40px',
+	marginTop: '0px',
+};
+
+const activeButton = {
+	justifyContent: 'center',
+	textAlign: 'center',
+	lineHeight: '2vw',
+	display: 'block',
+	height: '6vw',
+	width: '6vw',
+	backgroundColor: '#C7C7C7',
+	borderRadius: '10%',
+	marginBottom: '-1vw',
+	fontSize: '40px',
+	zIndex: '20000',
+	marginTop: '0px',
+};
+
+export default ProfileMenu;
